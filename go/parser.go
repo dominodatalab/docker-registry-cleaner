c5f49c9a59c5def4166d77775e49624da7df8024
Andrey Petrov <andrey.petrov@dominodatalab.com>
2024-01-09 17:47:07 -0600

    collect and print usage summary

--- go/parser.go
+++ go/parser.go
@@ -5,29 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
-	"time"
 )
 
 const dockerHost = "946429944765.dkr.ecr.us-west-2.amazonaws.com"
-
-type ImageMeta struct {
-	Name          string            `json:"Name"`
-	Digest        string            `json:"Digest"`
-	RepoTags      []string          `json:"RepoTags"`
-	Created       time.Time         `json:"Created"`
-	DockerVersion string            `json:"DockerVersion"`
-	Labels        map[string]string `json:"Labels"`
-	Architecture  string            `json:"Architecture"`
-	Os            string            `json:"Os"`
-	Layers        []string          `json:"Layers"`
-	LayersData    []struct {
-		MIMEType    string      `json:"MIMEType"`
-		Digest      string      `json:"Digest"`
-		Size        int         `json:"Size"`
-		Annotations interface{} `json:"Annotations"`
-	} `json:"LayersData"`
-	Env []string `json:"Env"`
-}
 
 func readTags(envName string, repoName string) ([]string, error) {
 	registry := fmt.Sprintf("docker://%s/%s/%s", dockerHost, envName, repoName)
@@ -40,42 +20,36 @@ func readTags(envName string, repoName string) ([]string, error) {
 	if err := cmd.Start(); err != nil {
 		return nil, err
 	}
-	type tagList struct {
-		Repository string
-		Tags       []string
+	var tagList struct {
+		Repository string   `json:"Repository"`
+		Tags       []string `json:"Tags"`
 	}
-	var tags tagList
-	if err := json.NewDecoder(stdout).Decode(&tags); err != nil {
+	if err := json.NewDecoder(stdout).Decode(&tagList); err != nil {
 		return nil, err
 	}
 	if err := cmd.Wait(); err != nil {
 		return nil, err
 	}
-	return tags.Tags, nil
+	return tagList.Tags, nil
 }
 
-func readImageMetadata(envName string, repoName string, tag string) (ImageMeta, error) {
+func readImageData(envName string, repoName string, tag string) (ImageData, error) {
 	registry := fmt.Sprintf("docker://%s/%s/%s:%s", dockerHost, envName, repoName, tag)
 	cmd := exec.Command("skopeo", "inspect", registry)
 	log.Printf("Executing '%s'\n", cmd)
-	var imageMeta ImageMeta
+	var image ImageData
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return imageMeta, err
+		return image, err
 	}
 	if err := cmd.Start(); err != nil {
-		return imageMeta, err
+		return image, err
 	}
-	//buf, err := io.ReadAll(stdout)
-	//if err != nil {
-	//	return "", err
-	//}
-	//return string(buf), nil
-	if err := json.NewDecoder(stdout).Decode(&imageMeta); err != nil {
-		return imageMeta, err
+	if err := json.NewDecoder(stdout).Decode(&image); err != nil {
+		return image, err
 	}
 	if err := cmd.Wait(); err != nil {
-		return imageMeta, err
+		return image, err
 	}
-	return imageMeta, nil
+	return image, nil
 }
